41124cc715173f2e3833fa356f1c519234fb1d5c
MaxWesley <mx_wesley@hotmail.com>
2022-03-15 00:22:40 -0300

    feat: define root and create first state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import {
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue
+} from 'recoil'
 
 function App() {
 
   return (
     <div className="App">
-      <h2>REACT</h2>
+      <TextInput />
     </div>
   )
 }
 
+const textState = atom({
+  key: 'textState',
+  default: ''  
+})
+
+function TextInput() {
+  const [text, setText] = useRecoilState<string>(textState);
+
+  const onChange = (event: any) => {
+    setText(event.target.value);
+  }
+
+  return(
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  )
+}
 export default App
